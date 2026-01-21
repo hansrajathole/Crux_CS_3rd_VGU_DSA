cd50e3622e94420e3ade1581ee55c8c05cbcfdec
hansrajathole <hansrajathole1@gmail.com>
2026-01-21 19:03:36 +0530

    loops and Patterns

---
+++ Lec4/Pattern2.java
@@ -0,0 +1,26 @@
+package Lec4;
+
+public class Pattern2 {
+    
+    public static void main(String[] args) {
+        int n = 5;
+        int star = 1;
+        int row = 1;
+
+    while(row<=n){
+
+        // Star
+        int i = 1;
+        while(i<= star) {
+            System.out.print("* ");
+            i++;
+        }
+
+        row++;
+        star++;
+        // next line prep
+        System.out.println();
+
+    }
+    }
+}
