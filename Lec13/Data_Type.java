377507871e7925ee06a3b9546870a1980cfc1131
hansrajathole <hansrajathole1@gmail.com>
2026-02-04 15:03:02 +0530

    data Types

---
+++ Lec13/Data_Type.java
@@ -0,0 +1,15 @@
+package Lec13;
+
+public class Data_Type {
+    
+
+    public static void main(String[] args) {
+        
+        int n = 278;
+
+        byte a = (byte)n;
+
+        System.out.println(a);
+
+    }
+}
